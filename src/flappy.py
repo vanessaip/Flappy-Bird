5dde069b21e9ca54dcc05dcb1f26a5900e01837a
vip02 <vip02@students.cs.ubc.ca>
2021-01-14 13:28:32 -0500

    playing is a boolean: only 2 game states

--- src/flappy.py
+++ src/flappy.py
@@ -3,13 +3,14 @@ import sys
 import random
 
 # Window constants
-SCREEN_HEIGHT = 700
+SCREEN_HEIGHT = 700     # changing SCREEN_HEIGHT scales the rest of the graphics as well
 SCALE = SCREEN_HEIGHT / 512     # original background image height
 SCREEN_WIDTH = round(288 * SCALE)
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-game_font = pygame.font.Font('04B_19.ttf', 40)
+game_font = pygame.font.Font('04B_19.ttf', round(37 * SCALE))
+game_font2 = pygame.font.Font('04B_19.ttf', round(40 * SCALE))
 
 
 # draws 2 floor images beside each other starting at floor_x
@@ -20,7 +21,10 @@ def draw_floor(floor_x):
 
 # draws bird at bird_rect
 def draw_bird(bird_index, bird_rect, movement):
-    img = pygame.transform.rotozoom(bird[bird_index], -movement * 4, 1)
+    angle = -movement * 4.5
+    if angle < -90:
+        angle = -90
+    img = pygame.transform.rotozoom(bird[bird_index], angle, 1)
     screen.blit(img, bird_rect)
 
 
@@ -31,6 +35,15 @@ def draw_pipes(pipe_rects):
             screen.blit(pipe, p)     # pipe is a rect (coordinates)
         else:
             screen.blit(pygame.transform.flip(pipe, False, True), p)
+
+
+# draws score
+def draw_score():
+    text = game_font.render(str(score), True, (255, 255, 255))
+    shadow = game_font2.render(str(score), True, (0, 0, 0))
+
+    screen.blit(shadow, (SCREEN_WIDTH / 2 + 1, SCREEN_HEIGHT / 6))
+    screen.blit(text, (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 6))
 
 
 # returns rects for new pair of pipes   !!! make resizable
@@ -66,10 +79,14 @@ def collide_pipe_left(rect, rect_list):
         return False
 
 
+# increments score if the centerx of one of pipe_rects is within 2 pixels of BIRD_X, otherwise does nothing
 def check_score(pipe_rects):
-    for p in pipe_rects:
-        if p.centerx == BIRD_X:
+    global score
+
+    for p in pipe_rects[::2]:       # every other in pipe_rects
+        if BIRD_X - 2 < p.centerx < BIRD_X + 2:
             score += 1
+            score_sound.play()
 
 
 # returns scaled image with filename
@@ -91,7 +108,6 @@ flap_sound = pygame.mixer.Sound('sound/sfx_wing.wav')
 collide_sound = pygame.mixer.Sound('sound/sfx_hit.wav')
 score_sound = pygame.mixer.Sound('sound/sfx_point.wav')
 
-
 # Rendering constants
 BIRD_X = SCREEN_WIDTH / 3
 BIRD_START_Y = SCREEN_HEIGHT / 3
@@ -104,7 +120,7 @@ SCROLL_SPEED = 3
 
 # Bird variables
 bird_index = 0
-bird_rect = bird[bird_index].get_rect(center=(BIRD_X, BIRD_START_Y))
+bird_rect = bird[bird_index].get_rect(center=(BIRD_X, BIRD_START_Y))        # used to manipulate bird position
 GRAVITY = 0.7       # TODO: make resizable 0.7, 9
 JUMP = 9
 movement = 0
@@ -114,7 +130,7 @@ pygame.time.set_timer(FLAP, 50)
 # Pipe variables
 pipe_rects = []
 SPAWN_PIPE = pygame.event.custom_type()
-pygame.time.set_timer(SPAWN_PIPE, 1200)     # TODO: resizable
+pygame.time.set_timer(SPAWN_PIPE, 1300)     # TODO: resizable original: 1200
 
 # Floor x pos
 floor_x = 0
@@ -131,8 +147,9 @@ def draw():
     draw_pipes(pipe_rects)
     draw_floor(floor_x)
     draw_bird(bird_index, bird_rect, movement)
+    draw_score()
 
-
+# TODO: multiple hit sounds/continuing even after collision if space bar is pressed
 while True:
     # Event handling
     for event in pygame.event.get():
@@ -154,8 +171,12 @@ while True:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     playing = True
+                    bird_rect.centery = BIRD_START_Y
+                    movement = 0
+                    pipe_rects.clear()
+                    score = 0
 # Continuous stuff
-    if bird_rect.bottom < FLOOR_Y - 5:
+    if bird_rect.bottom < FLOOR_Y - 15:
         # update bird rect
         movement += GRAVITY
         bird_rect.centery += movement
